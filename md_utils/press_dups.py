4a2d4bde602e39abefa7b80496dd5a0310bd9a1e
Chris Mayes <cmayes@cmayes.com>
2015-11-21 18:07:23 -0600

    Try just avg_rows.

--- md_utils/press_dups.py
+++ md_utils/press_dups.py
@@ -10,11 +10,11 @@ import logging
 import sys
 #from six.moves import reduce
 from functools import reduce
-from collections import defaultdict
+#from collections import defaultdict
 
 import numpy as np
 
-import common
+#import common
 
 
 logger = logging.getLogger(__name__)
@@ -44,70 +44,70 @@ def avg_rows(rows):
     return {k: (reduce(np.add, v) / len(v)) for k, v in agg_vals.items()}
 
 
-def compress_dups(data, column):
-    """Compresses rows that have the same value for the given column key,
-    averaging the values for the other entries.
-
-    :param data: The data to compress.
-    :param column: The column to search for duplicates.
-    :return: The data compressed for duplicates and sorted by the target
-        column's values.
-    """
-    idx = defaultdict(list)
-    for row in data:
-        idx[row[column]].append(row)
-
-    dedup = []
-
-    for idx_row in sorted(idx.items()):
-        dedup.append(avg_rows(idx_row[1]))
-    return dedup
-
-
-# CLI Processing #
-
-
-def parse_cmdline(argv=None):
-    """
-    Returns the parsed argument list and return code.
-    :param argv: A list of arguments, or `None` for ``sys.argv[1:]``.
-    """
-    if argv is None:
-        argv = sys.argv[1:]
-
-    # initialize the parser object:
-    parser = argparse.ArgumentParser(description='Compresses duplicate rows in a '
-                                                 'given file based on values from '
-                                                 'a given column')
-
-    parser.add_argument('-c', '--column', default=DEF_COL_NAME,
-                        help="Specify dupe column. (defaults to {})".format(DEF_COL_NAME),
-                        metavar="DUPE_COL")
-    parser.add_argument("file", help="The CSV file to process")
-    args = parser.parse_args(argv)
-
-    return args, 0
-
-
-def main(argv=None):
-    """
-    Runs the main program.
-
-    :param argv: The command line arguments.
-    :return: The return code for the program's termination.
-    """
-    args, ret = parse_cmdline(argv)
-    if ret != 0:
-        return ret
-
-    deduped = compress_dups(common.read_csv(args.file, all_conv=float), args.column)
-    fmt_deduped = common.fmt_row_data(deduped, "{:.6f}")
-    common.write_csv(fmt_deduped, common.create_out_fname(args.file, PREFIX),
-                     common.read_csv_header(args.file))
-
-    return 0  # success
-
-
-if __name__ == '__main__':
-    status = main()
-    sys.exit(status)
+# def compress_dups(data, column):
+#     """Compresses rows that have the same value for the given column key,
+#     averaging the values for the other entries.
+#
+#     :param data: The data to compress.
+#     :param column: The column to search for duplicates.
+#     :return: The data compressed for duplicates and sorted by the target
+#         column's values.
+#     """
+#     idx = defaultdict(list)
+#     for row in data:
+#         idx[row[column]].append(row)
+#
+#     dedup = []
+#
+#     for idx_row in sorted(idx.items()):
+#         dedup.append(avg_rows(idx_row[1]))
+#     return dedup
+#
+#
+# # CLI Processing #
+#
+#
+# def parse_cmdline(argv=None):
+#     """
+#     Returns the parsed argument list and return code.
+#     :param argv: A list of arguments, or `None` for ``sys.argv[1:]``.
+#     """
+#     if argv is None:
+#         argv = sys.argv[1:]
+#
+#     # initialize the parser object:
+#     parser = argparse.ArgumentParser(description='Compresses duplicate rows in a '
+#                                                  'given file based on values from '
+#                                                  'a given column')
+#
+#     parser.add_argument('-c', '--column', default=DEF_COL_NAME,
+#                         help="Specify dupe column. (defaults to {})".format(DEF_COL_NAME),
+#                         metavar="DUPE_COL")
+#     parser.add_argument("file", help="The CSV file to process")
+#     args = parser.parse_args(argv)
+#
+#     return args, 0
+#
+#
+# def main(argv=None):
+#     """
+#     Runs the main program.
+#
+#     :param argv: The command line arguments.
+#     :return: The return code for the program's termination.
+#     """
+#     args, ret = parse_cmdline(argv)
+#     if ret != 0:
+#         return ret
+#
+#     deduped = compress_dups(common.read_csv(args.file, all_conv=float), args.column)
+#     fmt_deduped = common.fmt_row_data(deduped, "{:.6f}")
+#     common.write_csv(fmt_deduped, common.create_out_fname(args.file, PREFIX),
+#                      common.read_csv_header(args.file))
+#
+#     return 0  # success
+#
+#
+# if __name__ == '__main__':
+#     status = main()
+#     sys.exit(status)
--- tests/test_press_dups.py
+++ tests/test_press_dups.py
@@ -20,8 +20,7 @@ import unittest
 
 import os
 
-from md_utils import press_dups
-from md_utils.common import diff_lines, silent_remove
+from md_utils.press_dups import avg_rows
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'test_data')
 DUPS_DIR = os.path.join(DATA_DIR, 'press_dups')
@@ -32,15 +31,15 @@ WHIT_PRESS = os.path.join(DUPS_DIR, 'pressed_' + 'proc_data_all_withhead0.75.csv
 # # Shared Methods #
 #
 #
-# class TestAvgRows(unittest.TestCase):
-#     def testThree(self):
-#         data = [{"a": 1.3, "b": 3.0, "c": 8.5}, {"a": 1.3, "b": 1.0, "c": -4.2},
-#                 {"a": 1.3, "b": 2.2, "c": 19.0}]
-#         avg = avg_rows(data)
-#         self.assertAlmostEqual(avg['a'], 1.3)
-#         self.assertAlmostEqual(avg['b'], 2.066666666666)
-#         self.assertAlmostEqual(avg['c'], 7.766666666666)
-#
+class TestAvgRows(unittest.TestCase):
+    def testThree(self):
+        data = [{"a": 1.3, "b": 3.0, "c": 8.5}, {"a": 1.3, "b": 1.0, "c": -4.2},
+                {"a": 1.3, "b": 2.2, "c": 19.0}]
+        avg = avg_rows(data)
+        self.assertAlmostEqual(avg['a'], 1.3)
+        self.assertAlmostEqual(avg['b'], 2.066666666666)
+        self.assertAlmostEqual(avg['c'], 7.766666666666)
+
 #
 # class TestPressDups(unittest.TestCase):
 #     def testThree(self):
