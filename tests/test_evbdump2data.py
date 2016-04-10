ad9440a9c870473cf880df649f808b280dcf0888
Heather Mayes <hmayes@hmayes.com>
2016-04-10 12:28:33 -0500

    Lots of files for testing md_utils/evbdump2data.py
    Former-commit-id: 9a2d191e459bfea095e5bde05f9dc6f905470392

--- tests/test_evbdump2data.py
+++ tests/test_evbdump2data.py
@@ -13,33 +13,42 @@ from md_utils.md_common import capture_stdout, capture_stderr, diff_lines, silen
 __author__ = 'hmayes'
 
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), 'test_data')
+TEST_DIR = os.path.dirname(__file__)
+DATA_DIR = os.path.join(TEST_DIR, 'test_data')
 SUB_DATA_DIR = os.path.join(DATA_DIR, 'evbd2d')
 
-INCOMP_INI_PATH = os.path.join(SUB_DATA_DIR, 'evbd2d_missing_data.ini')
-GOOD_INI_PATH = os.path.join(SUB_DATA_DIR, 'evbd2d_good.ini')
-GOOD_INI_NO_CHARGE_PATH = os.path.join(SUB_DATA_DIR, 'evbd2d_good_no_section_atom_nums.ini')
+# Test input files
 
-GOOD_PROT_OUT_PATH = os.path.join(SUB_DATA_DIR, 'serca_prot_good.data')
-GOOD_DEPROT_OUT_PATH = os.path.join(SUB_DATA_DIR, 'serca_deprot_good.data')
+SERCA_INI = os.path.join(SUB_DATA_DIR, 'evbd2d_serca.ini')
+INCOMP_INI = os.path.join(SUB_DATA_DIR, 'evbd2d_missing_data.ini')
+TPL_DUMP_MISMATCH_INI = os.path.join(SUB_DATA_DIR, 'evbd2d_glu_tpl_serca_dump.ini')
+TPL_MISS_ATOM_INI = os.path.join(SUB_DATA_DIR, 'evbd2d_glu_tpl_missing_atom.ini')
+TPL_WRONG_ATOM_ORDER_INI = os.path.join(SUB_DATA_DIR, 'evbd2d_glu_tpl_wrong_atom_order.ini')
 
-GLU_INI_PATH = os.path.join(SUB_DATA_DIR, 'evbd2d_glu.ini')
-GLU_NEW_INI_PATH = os.path.join(SUB_DATA_DIR, 'evbd2d_glu_new_types.ini')
-GLU_BAD_INI_PATH = os.path.join(SUB_DATA_DIR, 'evbd2d_glu_bad.ini')
+# evbd2d_glu_tpl_missing_atom.ini
 
-GLU_DEF_OUT = os.path.join(SUB_DATA_DIR, '0.625_20c_100ps_reorder_retype_548990.data')
-GLU_GOOD_OUT = os.path.join(SUB_DATA_DIR, '0.625_20c_100ps_reorder_retype_548990_good.data')
+GLU_INI = os.path.join(SUB_DATA_DIR, 'evbd2d_glu.ini')
+GLU_NEW_INI = os.path.join(SUB_DATA_DIR, 'evbd2d_glu_new_types.ini')
+GLU_BAD_ATOM_TYPE_INI = os.path.join(SUB_DATA_DIR, 'evbd2d_glu_type_mismatch.ini')
+GLU_BAD_DATA_INI = os.path.join(SUB_DATA_DIR, 'evbd2d_glu_bad_dump.ini')
+
 
 # Test output files #
 
+GOOD_PROT_OUT = os.path.join(SUB_DATA_DIR, 'serca_prot_good.data')
+GOOD_DEPROT_OUT = os.path.join(SUB_DATA_DIR, 'serca_deprot_good.data')
+
 # noinspection PyUnresolvedReferences
-REPROD_TPL = os.path.join(DATA_DIR, 'reproduced_tpl.data')
+GLU_DEF_OUT = os.path.join(SUB_DATA_DIR, '0.625_20c_100ps_reorder_retype_548990.data')
+GLU_GOOD_OUT = os.path.join(SUB_DATA_DIR, '0.625_20c_100ps_reorder_retype_548990_good.data')
+GLU_DATA_TPL = os.path.join(SUB_DATA_DIR, 'glue_tpl.data')
+
 # noinspection PyUnresolvedReferences
-REPROD2_TPL = os.path.join(os.path.dirname(__file__), 'reproduced_tpl.data')
+REPROD_TPL = os.path.join(TEST_DIR, 'reproduced_tpl.data')
 # noinspection PyUnresolvedReferences
-DEF_PROT_OUT_PATH = os.path.join(SUB_DATA_DIR, 'serca_prot_deprot_10.data')
+DEF_PROT_OUT = os.path.join(SUB_DATA_DIR, 'serca_prot_deprot_10.data')
 # noinspection PyUnresolvedReferences
-DEF_DEPROT_OUT_PATH = os.path.join(SUB_DATA_DIR, 'serca_prot_deprot_20.data')
+DEF_DEPROT_OUT = os.path.join(SUB_DATA_DIR, 'serca_prot_deprot_20.data')
 
 
 class TestEVBDump2Data(unittest.TestCase):
@@ -50,65 +59,80 @@ class TestEVBDump2Data(unittest.TestCase):
             self.assertTrue("Problems reading file: Could not read file" in output)
 
     def testMissingConfig(self):
-        with capture_stderr(evbdump2data.main, ["-c", INCOMP_INI_PATH]) as output:
+        with capture_stderr(evbdump2data.main, ["-c", INCOMP_INI]) as output:
             self.assertTrue("Input data missing" in output)
-        with capture_stdout(evbdump2data.main, ["-c", INCOMP_INI_PATH]) as output:
+        with capture_stdout(evbdump2data.main, ["-c", INCOMP_INI]) as output:
             self.assertTrue("optional arguments" in output)
 
-    def testProt(self):
-        with capture_stdout(evbdump2data.main, ["-c", GOOD_INI_PATH]) as output:
-            # Checking intermediate charge calculation
-            self.assertTrue("After atom 15436 (last_p1), the total charge is: -26.000" in output)
-        try:
-            self.assertFalse(diff_lines(DEF_PROT_OUT_PATH, GOOD_PROT_OUT_PATH))
-        finally:
-            silent_remove(DEF_PROT_OUT_PATH)
-            silent_remove(DEF_DEPROT_OUT_PATH)
-            silent_remove(REPROD2_TPL)
-
-    def testDeprot(self):
-        with capture_stderr(evbdump2data.main, ["-c", GOOD_INI_PATH]) as output:
+    def testSercaProtDeprot(self):
+        """
+        Tests a SERCA file that has a protonated and deprotonated state
+        Also checks that can handle and empty line
+        and a dump file that was cut off
+        """
+        with capture_stderr(evbdump2data.main, ["-c", SERCA_INI]) as output:
             # Make sure it handles the extra empty line
             self.assertFalse("WARNING:  Problems reading file" in output)
-        try:
-            self.assertFalse(diff_lines(DEF_PROT_OUT_PATH, GOOD_PROT_OUT_PATH))
-            self.assertFalse(diff_lines(DEF_DEPROT_OUT_PATH, GOOD_DEPROT_OUT_PATH))
-        finally:
-            silent_remove(DEF_PROT_OUT_PATH)
-            silent_remove(DEF_DEPROT_OUT_PATH)
-            silent_remove(REPROD2_TPL)
-
-    def testNoChargeCheck(self):
-        with capture_stdout(evbdump2data.main, ["-c", GOOD_INI_NO_CHARGE_PATH]) as output:
-            # Checking intermediate charge calculation
-            self.assertTrue("system is neutral" in output)
-        silent_remove(REPROD_TPL)
-
-    def testIncompDump(self):
-        with capture_stderr(evbdump2data.main, ["-c", GOOD_INI_NO_CHARGE_PATH]) as output:
-            # Make sure it handles the extra empty line
             self.assertTrue("did not have the full list of atom numbers" in output)
         try:
-            self.assertFalse(diff_lines(DEF_PROT_OUT_PATH, GOOD_PROT_OUT_PATH))
-            self.assertFalse(diff_lines(DEF_DEPROT_OUT_PATH, GOOD_DEPROT_OUT_PATH))
+            self.assertFalse(diff_lines(DEF_PROT_OUT, GOOD_PROT_OUT))
+            self.assertFalse(diff_lines(DEF_DEPROT_OUT, GOOD_DEPROT_OUT))
         finally:
-            silent_remove(DEF_PROT_OUT_PATH)
-            silent_remove(DEF_DEPROT_OUT_PATH)
-            silent_remove(REPROD_TPL)
+            silent_remove(DEF_PROT_OUT)
+            silent_remove(DEF_DEPROT_OUT)
 
     def testNoWaterDump(self):
-        with capture_stderr(evbdump2data.main, ["-c", GLU_NEW_INI_PATH]) as output:
+        with capture_stderr(evbdump2data.main, ["-c", GLU_NEW_INI]) as output:
             self.assertTrue("Found no water molecules" in output)
         silent_remove(REPROD_TPL)
 
     def testNoHydData(self):
-        with capture_stderr(evbdump2data.main, ["-c", GLU_BAD_INI_PATH]) as output:
+        """
+        Because the types do not match the data file, did not find the molecules desired.
+        """
+        evbdump2data.main(["-c", GLU_BAD_ATOM_TYPE_INI])
+        with capture_stderr(evbdump2data.main, ["-c", GLU_BAD_ATOM_TYPE_INI]) as output:
             self.assertTrue("Check the data file" in output)
         silent_remove(REPROD_TPL)
 
     def testGlu(self):
         try:
-            evbdump2data.main(["-c", GLU_INI_PATH])
-            self.assertFalse(diff_lines(GLU_DEF_OUT, GLU_GOOD_OUT))
+            with capture_stdout(evbdump2data.main, ["-c", GLU_INI]) as output:
+                # Checking intermediate charge calculation
+                self.assertTrue("After atom 27 (last_p1), the total charge is: -1.000" in output)
+                self.assertTrue("system is neutral" in output)
+                self.assertTrue("After atom 31 (last_hyd), the total charge is" in output)
+                self.assertTrue(GLU_DEF_OUT in output)
+                self.assertFalse(diff_lines(GLU_DEF_OUT, GLU_GOOD_OUT))
+                self.assertTrue(REPROD_TPL in output)
+                self.assertFalse(diff_lines(REPROD_TPL, GLU_DATA_TPL))
+        finally:
+            silent_remove(GLU_DEF_OUT)
+            silent_remove(REPROD_TPL)
+
+    def testGluBadDump(self):
+        try:
+            with capture_stderr(evbdump2data.main, ["-c", GLU_BAD_DATA_INI]) as output:
+                self.assertTrue("Problems reading data: Unexpected line in file test_data/evbd2d/glu_bad.dump: "
+                                "ITEM: XYZ MS id mol type q x y z" in output)
         finally:
             silent_remove(REPROD_TPL)
+
+    def testTplDumpMismatch(self):
+        try:
+            with capture_stderr(evbdump2data.main, ["-c", TPL_DUMP_MISMATCH_INI]) as output:
+                self.assertTrue("listed number of atoms (214084) does not equal the number of atoms in the "
+                                "template data file (1429)" in output)
+        finally:
+            silent_remove(REPROD_TPL)
+
+    def testTplMissAtom(self):
+        with capture_stderr(evbdump2data.main, ["-c", TPL_MISS_ATOM_INI]) as output:
+                self.assertTrue("The data file system is not neutral. Total charge -0.510000" in output)
+                self.assertTrue("The data file system is not neutral. Total charge -0.510000" in output)
+
+    def testMissingArg(self):
+        evbdump2data.main(["-c", ])
+
+    def testTplAtomWrongOrder(self):
+        evbdump2data.main(["-c", TPL_WRONG_ATOM_ORDER_INI])
