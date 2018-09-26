56e1be764293419a8f34090c2e6db9cc5db59d98
xadams <xadams@umich.edu>
2018-09-26 11:01:25 -0400

    Added additional options to bcolors for future use

--- md_utils/CV_analysis.py
+++ md_utils/CV_analysis.py
@@ -21,8 +21,14 @@ __author__ = 'xadams'
 
 
 class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
     BOLD = '\033[1m'
     ENDC = '\033[0m'
+    UNDERLINE = '\033[4m'
 
 
 HOME = str(Path.home())
